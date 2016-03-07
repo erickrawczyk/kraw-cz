966615cb416a8d4e9c281372eab085a6bba3a6cd
Eric Krawczyk <eric@docnetwork.org>
2016-03-07 10:21:29 -0500

    img: update background images
    - move /img out of /dist
    - add gulp 'img' task which copies img to dist
    - add new background images for each section
    - ignore /dist since it can be built with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,29 @@ var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var concat = require('gulp-concat');
 
-gulp.task('watch', ['build'], function () {
+gulp.task('watch', ['build'], () => {
   gulp.watch(['*.html'], { interval: 1000}, ['html']);
   gulp.watch(['styles/*.scss'], { interval: 1000 }, ['css']);
   gulp.watch(['js/*.js'], { interval: 1000 }, ['js']);
+  gulp.watch(['img/*'], { interval: 1000 }, ['img']);
 });
 
-gulp.task('build', ['html', 'css', 'js']);
+gulp.task('build', ['html', 'css', 'js', 'img']);
 
-gulp.task('html', function() {
+gulp.task('html', () => {
   return gulp.src('*.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('css', function() {
+gulp.task('css', () => {
   return gulp.src('styles/*.scss')
   .pipe(concat('styles.css'))
   .pipe(sass({outputStyle: 'compressed'}))
   .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('js', function() {
+gulp.task('js', () => {
   return gulp.src([
     'js/jquery.js',
     'js/bootstrap.js',
@@ -34,6 +35,10 @@ gulp.task('js', function() {
   ]).pipe(concat('main.js'))
     .pipe(uglify({mangle:true}))
     .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('img', () => {
+  return gulp.src('img/*').pipe(gulp.dest('dist/img'))
 });
 
 gulp.task('default', ['build']);
