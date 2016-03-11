832eb928ec39a3716eb3497d259f1a3ce3ada18e
Eric Krawczyk <eric@docnetwork.org>
2016-03-10 19:20:14 -0500

    okay, pace was kind of silly

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,26 +1,23 @@
 (function puzzle(){
-
-  console.log("%cWelcome to DevTools!", "color:red; font-size: 18px;");
-  console.log("%cThere might be a clue() lying around!", "color:blue; font-size: 14px;");
+  console.log("%cSnooping around, are you?", "color:red; font-size: 24px;");
+  console.log("%cAre you one of those %cdevelopers?", "color:blue; font-size: 18px;", "color:red; font-size: 18px; font-style:italic;");
+  console.log("%cFollow the trail of breadcrumbs() and you might find something!", "color:green; font-size: 14px;");
 })();
 
-function clue() {
+function breadcrumbs() {
   var rand = Math.round(Math.random() * 3);
   switch (rand) {
     case 0:
-      console.log('%c' + "I lied I haven't built this yet", "color: red;");
+      console.log('%c' + "I'm sorry I lied I haven't built this yet :(", "color: red;");
       break;
     case 1:
-      console.log('%c' + "I lied I haven't built this yet", "color: red;");
-      console.log('');
+      console.log('%c' + "I'm sorry I lied I haven't built this yet :(", "color: red;");
       break;
     case 2:
-      console.log('%c' + "I lied I haven't built this yet", "color: red;");
-      console.log('');
+      console.log('%c' + "I'm sorry I lied I haven't built this yet :(", "color: red;");
       break;
     case 3:
-      console.log('%c' + "I lied I haven't built this yet", "color: red;");
-      console.log('');
+      console.log('%c' + "I'm sorry I lied I haven't built this yet :(", "color: red;");
       break;
   }
   return
